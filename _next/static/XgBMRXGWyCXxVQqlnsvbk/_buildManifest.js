f8113f2f8caf5b43480d10d7d50f3cc9340eee95
cksqls97 <cksqls97@users.noreply.github.com>
2026-02-06 04:44:11 +0000

    deploy: 759e045ca77ea60cac862d35f5c59d070532cf1e

---
+++ _next/static/XgBMRXGWyCXxVQqlnsvbk/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/loa_for_me//_next/:path+",
+        "destination": "/loa_for_me/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
